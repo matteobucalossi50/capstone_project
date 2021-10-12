be74ad5e1a52031f126944c8d6a36df0f2850f27
Michael Pagan <pagan.michael51@gmail.com>
2021-10-12 09:33:22 -0400

    show constants

---
+++ constants/constants.py
@@ -0,0 +1,12 @@
+def get_mp_api_key():
+    # return 'c7f971117b34db96607a989c10f0d985'
+    return '53b7426a41102cb6e217635a882c36d1'
+    # return 'fb1be0d8f88649210207adab97853f34'
+    
+def get_matteo_twitter_creds():
+    access_token = '2559294522-9beAFYriWwSdFBnpGZMlEuiFmLwWFsU9yDyXlXU'
+    # bearer 'AAAAAAAAAAAAAAAAAAAAACKwSAEAAAAAy%2FFd2hCP7rY9j61xMjWhvg1MsHk%3DlMS19IuX2vpYNU7L2hP33P5eNc7MoHvIT5Hc6QagASmEcKYstw'
+    access_token_secret = 'x2eNxpidxxqLSbLvL4eYpoEaBTRQM0eYD8keqr3YnZxlL'
+    consumer_key = '57HDK4QvI5XCZn4Dq2Oa27vdN'
+    consumer_secret = 'bM59AFPQy9jqI8xp7yqBb6zNUGVcdzIsqdTIyuHOLlNvmXzIJ9'
+    return access_token, access_token_secret, consumer_key, consumer_secret

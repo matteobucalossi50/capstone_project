ab0089bf6d770398f7339e11dc0bda756ea2aceb
Michael Pagan <pagan.michael51@gmail.com>
2021-10-16 16:18:28 -0400

    feat: add scrape for food vocab list

--- scraping/scrape_hashtags.py
+++ scraping/scrape_hashtags.py
@@ -2,11 +2,47 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
 from datetime import datetime
 import os
 
-def get_hashtag_stats(hashtag_list):
+# Chrome driver
+CHROME_PATH = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
+CHROMEDRIVER_PATH = r'C:\Users\raide\OneDrive\Documents\GitHub\capstone_project\scraping\chromedriver_win32\chromedriver'
+WINDOW_SIZE = '1920,1080'
+
+# Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.binary_location = CHROME_PATH
+
+# Begin Selenium service
+# service = Service(r'C:\Users\raide\OneDrive\Documents\GitHub\capstone_project\scraping\chromedriver_win32\chromedriver')
+# service.start()
+
+# Initialize Driver
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+
+#%%
+def enchanted_learning_food(driver=driver):
+    
+    # Navigate to page
+    url = 'https://www.enchantedlearning.com/wordlist/food.shtml'
+    driver.get(url)
+    time.sleep(2)
+
+    # Create list of words from page
+    word_list_from_browser = driver.find_elements_by_class_name('wordlist-item')
+    raw_word_list = [s.text for s in word_list_from_browser]
+    # hashtags_from_word_list = ['#' + s.replace(' ', '') for s in raw_word_list]
+
+    return raw_word_list
+
+#%%
+
+def get_hashtag_stats(driver=driver):
     """
     Visits https://ritetag.com/hashtag-comparison/ to get statistics about the supplied hashtags.
     
@@ -19,10 +55,7 @@ def get_hashtag_stats(hashtag_list):
     df (DataFrame): A pandas DataFrame of the result.
     """
     
-    # Begin Selenium service
-    service = Service(r'C:\Users\raide\OneDrive\Documents\GitHub\capstone_project\scraping\chromedriver_win32\chromedriver')
-    service.start()
-    driver = webdriver.Remote(service.service_url)
+    hashtag_list = enchanted_learning_food()
 
     # Create URL
     hashtag_url_list = [s + '%7C' if i != len(hashtag_list) - 1 else s for i, s in enumerate(hashtag_list)]
@@ -50,17 +83,22 @@ def get_hashtag_stats(hashtag_list):
     # quit driver
     driver.quit()
 
-    data = {'hashtag': hashtag_list_from_browser, 'unique_tweets_per_hour': unique_tweets_per_hour, 'retweets_per_hour': retweets_per_hour, 'views_per_hour': hashtag_exposure_per_hour}
+    data = {'hashtag': hashtag_list_from_browser[:len(unique_tweets_per_hour)], 'unique_tweets_per_hour': unique_tweets_per_hour, 'retweets_per_hour': retweets_per_hour, 'views_per_hour': hashtag_exposure_per_hour}
 
     # build dataframe
     df = pd.DataFrame(data)
     df = df.replace(',', '', regex=True)
     df[['unique_tweets_per_hour', 'retweets_per_hour', 'views_per_hour']] = df[['unique_tweets_per_hour', 'retweets_per_hour', 'views_per_hour']].apply(pd.to_numeric)
     to_csv_timestamp = datetime.today().strftime('%Y%m%d_%H%M%S_')
-    path = r'C:\Users\raide\OneDrive\Documents\GitHub\capstone_project\data'
+    path = r'C:\Users\raide\OneDrive\Documents\GitHub\capstone_project\data\hashtag_stats'
     filename = path + '\\' + to_csv_timestamp + 'hashtag_stats.csv'
     df.to_csv(filename, index=False)
     
     return df
+
+# %%
+# raw_word_list, hashtags_from_word_list = enchanted_learning_food()
+# df = get_hashtag_stats(raw_word_list)
+# df
 
 # %%
